e4cadee62384b98a046b7c4d1e8261661b68ada1
Kat Hogan <32649279+katkola@users.noreply.github.com>
2022-10-23 20:56:57 -0400

    Changes to be committed: 	modified:   flask_app/controllers/surveyController.py 	new file:   flask_app/static/css/bootstrap.css 	modified:   flask_app/templates/index.html 	new file:   flask_app/templates/results.html

--- flask_app/controllers/surveyController.py
+++ flask_app/controllers/surveyController.py
@@ -1,6 +1,18 @@
 from flask_app import app
-from flask import redirect, render_template, request
+from flask import redirect, render_template, request, session
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/results', methods=['POST'])
+def results():
+    print('Thanks')
+    print(request.form)
+    session['name'] = request.form['name']
+    session['dojo'] = request.form['dojo']
+    session['favLang'] = request.form['favLang']
+    session['comment'] = request.form['comment']
+
+    return render_template('results.html')
+
